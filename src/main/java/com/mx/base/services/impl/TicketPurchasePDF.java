b5de0655651eea1debb366dc854d8c5eafd3fe2f
edbercruz <cupedber.126@gmail.com>
2019-12-17 20:09:05 -0600

    fix download

--- src/main/java/com/mx/base/services/impl/TicketPurchasePDF.java
+++ src/main/java/com/mx/base/services/impl/TicketPurchasePDF.java
@@ -2,6 +2,10 @@ package com.mx.base.services.impl;
 
 import java.awt.Desktop;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -9,6 +13,7 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.stereotype.Service;
 
 import com.mx.base.models.catalog.TicketPDF;
@@ -19,6 +24,22 @@ public class TicketPurchasePDF {
 
 	private String filePathtTickets = "/home/milla/purchase/tickets";
 	
+	public void download(HttpServletResponse response,TicketPDF ticketPDF, int year) {
+		try {
+			String fileName = "Ticket_"+ticketPDF.getLotNumber()+".pdf";
+			
+			File fileToDownload = new File(filePathtTickets+"/"+year+"/" + fileName);
+
+			InputStream inputStream = new FileInputStream(fileToDownload);
+			response.setContentType("application/force-download");
+			response.setHeader("Content-Disposition", "attachment; filename=" + fileName);
+			IOUtils.copy(inputStream, response.getOutputStream());
+			response.flushBuffer();
+			inputStream.close();
+		} catch (Exception exception) {
+			System.out.println(exception.getMessage());
+		}
+	}
 	public void makeTicketPDF(TicketPDF ticketPDF, int year){				
 		String fileName = "Ticket_"+ticketPDF.getLotNumber()+".pdf";
 		File ticketFile = new File(filePathtTickets +"/"+year+"/"+ fileName);	
@@ -88,7 +109,7 @@ public class TicketPurchasePDF {
 			ex.printStackTrace();
 	  }		
 	  */
-		DesktopAPI.open(ticketFile);
+	//	DesktopAPI.open(ticketFile);
 	}
 	
 }
